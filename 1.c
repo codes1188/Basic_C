503f581a9375a6674488fab2f6e9c0da51a06c9d
codes1188 <tum4856hs@gmail.com>
2025-01-29 20:19:58 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello Love!\n");
+    printf("How are you feeling?");
+    return 0;
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hii beautiful\nHow's it going?");//\n is used to change the line
+    return 0;
+}
